a8bf75e0f5556575d278286377dde6940a7c15cd
Dongxin Tian <kakitsubata0110@gmail.com>
2025-04-08 17:37:43 -0400

    Renamed to Actor2D

--- core/src/main/java/edu/duke/ece651/factorysim/Actor2D.java
+++ core/src/main/java/edu/duke/ece651/factorysim/Actor2D.java
@@ -1,15 +1,18 @@
 package edu.duke.ece651.factorysim;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
-public class Actor {
+public abstract class Actor2D {
     public final Vector2 position;
 
-    public Actor(float x, float y) {
+    public Actor2D(float x, float y) {
         this.position = new Vector2(x, y);
     }
 
-    public Actor() {
+    public Actor2D() {
         this.position = Vector2.Zero;
     }
+
+    public abstract void draw(SpriteBatch spriteBatch);
 }
