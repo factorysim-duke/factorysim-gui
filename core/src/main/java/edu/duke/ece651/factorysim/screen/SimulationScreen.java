2350d1f144df43784fe142304994c569531ac4e7
Xingyu Shen <shen13515251251@gmail.com>
2025-04-10 14:25:57 -0400

    create simulation screen class

---
+++ core/src/main/java/edu/duke/ece651/factorysim/screen/SimulationScreen.java
@@ -0,0 +1,41 @@
+package edu.duke.ece651.factorysim.screen;
+
+import com.badlogic.gdx.Screen;
+
+public class SimulationScreen implements Screen {
+
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void render(float delta) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+    }
+}
