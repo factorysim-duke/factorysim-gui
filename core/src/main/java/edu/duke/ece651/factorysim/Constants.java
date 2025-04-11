84ab0bb2fa7660ec6bb9bf7bb4bd8131f0e5ed81
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-04-11 02:56:20 -0400

    Added CELL_SIZE constant

--- core/src/main/java/edu/duke/ece651/factorysim/Constants.java
+++ core/src/main/java/edu/duke/ece651/factorysim/Constants.java
@@ -3,9 +3,11 @@ package edu.duke.ece651.factorysim;
 public class Constants {
     private Constants() { }
 
+    public static final int WINDOW_WIDTH = 1280;
+    public static final int WINDOW_HEIGHT = 720;
+
     public static final int VIEW_WIDTH = 640;
     public static final int VIEW_HEIGHT = 360;
 
-    public static final int WINDOW_WIDTH = 1280;
-    public static final int WINDOW_HEIGHT = 720;
+    public static final int CELL_SIZE = 16;
 }
