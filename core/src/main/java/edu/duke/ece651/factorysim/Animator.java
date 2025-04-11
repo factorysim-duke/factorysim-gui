7b7ba4282783de5b5bf59e4d9689101c37c13777
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-04-11 04:48:24 -0400

    Animator<T>

---
+++ core/src/main/java/edu/duke/ece651/factorysim/Animator.java
@@ -0,0 +1,46 @@
+package edu.duke.ece651.factorysim;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+
+/**
+ * Represents a simple timer-based animation controller for libGDX animations.
+ *
+ * @param <T> is the frame type.
+ */
+public class Animator<T> {
+    private final Animation<T> animation;
+
+    private final boolean looping;
+
+    private float timer;
+
+    /**
+     * Constructs an animator with the given animation and loop setting.
+     *
+     * @param animation is the animation to play.
+     * @param looping whether the animation should loop.
+     */
+    public Animator(Animation<T> animation, boolean looping) {
+        this.animation = animation;
+        this.looping = looping;
+        this.timer = 0f;
+    }
+
+    /**
+     * Advances the animation timer by the given delta time.
+     *
+     * @param dt time in seconds since last update.
+     */
+    public void step(float dt) {
+        timer += dt;
+    }
+
+    /**
+     * Gets the current frame based on the internal timer.
+     *
+     * @return the current animation frame.
+     */
+    public T getCurrentKeyFrame() {
+        return animation.getKeyFrame(timer, looping);
+    }
+}
